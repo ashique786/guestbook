c7f3213b76a5a4d07211100d7b9eecebb1c03f36
Ashique Rahman <pcashiquerahman@gmail.com>
2019-10-29 23:32:41 +0530

    create endpoints

--- backend/guestRoutes.js
+++ backend/guestRoutes.js
@@ -6,9 +6,37 @@ const parseurl = require('parseurl');
 const path = require('path');
 const expressValidator = require('express-validator');
 
-const db_url ='mongodb://ashique:surface313@ds239928.mlab.com:39928/guestbook';
-const Signature = require('./Singnatures');
+const db_url = process.env.MONGOLAB_URI;
+const Signature = require('./Signatures');
 const app = express();
 
+mongoose.connect(db_url, (err) => {
+    if (err) {
+        console.log('connection to mongoDB is not established error: ', err);
+    } else {
+        console.log('mongoDB connection is established at ', db_url);
+    }
 
+})
+app.get('/', (req, res) => {
+    res.json('Great, you have done it !!');
+});
 
+app.get('/guestbook', (req, res) => {
+    Signature.find({})
+        .then(allSignatures => {
+            res.json(allSignatures);
+        })
+
+})
+
+app.get('/createSignature', (req, res) => {
+    Signature.create({
+        guestSignature: req.body.signatureOfGuest,
+        message: req.body.messageOfGuest,
+    }).then(signature => {
+        res.json(signature);
+    })
+})
+
+app.listen(process.env.PORT || 3000);
